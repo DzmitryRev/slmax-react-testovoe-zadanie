030331bacac1ff9dc21dd358f1cfcb3fb824791d
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-06-22 23:21:14 +0300

    Implement Header and content container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className + " text-gray-700"}>
+				<Header />
+				<div className="max-w-[1180px] mx-auto px-2 pt-5">{children}</div>
+			</body>
 		</html>
 	);
 }
